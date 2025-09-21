d41853848446fc105f2597db7ec2718502210962
N-Nashe <munalechakona@gmail.com>
2025-09-21 14:37:38 +0200

    added format date function as well as import genres from data.js

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,51 @@
 import React, { useState, useEffect } from 'react'
+import {genres} from '../data.js'
 import './styles.css'
 
 function App() {
   
   const [podcasts, setPodcasts] = useState([])        
   const [loading, setLoading] = useState(true)        
-  const [error, setError] = useState(null)            
+  const [error, setError] = useState(null)  
+  
+  const getGenreNames = (genreIds) => {
+    return genreIds.map(id => {
+      const foundGenre = genres.find(genre => genre.id === id)
+      return foundGenre ? foundGenre.title : 'Unknown'
+    }).join(', ')
+  }
+  
+  const formatDate = (dateString) => {
+    const date = new Date(dateString)
+    return date.toLocaleDateString('en-US', { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+    })
+  }
+  
+  useEffect(() => {
+ const fetchPodcasts = async () => {
+    
+  try{
+    const response = await fetch ('https://podcast-api.netlify.app/')
+    
+    if (!response.ok){
+      throw new Error('Failed to load podcasts!')
+    }
+    const data = await response.json()
+    setPodcasts(data)
+  } catch (error){
+    setError(error.message)
+
+  } finally{
+    setLoading(false);
+  }
+
+  }
+
+  fetchPodcasts()
+ }, [])
 
   return (
     <div>
