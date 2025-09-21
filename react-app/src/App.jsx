1f96b54564c37c8b77374a39724ee050a31782f0
N-Nashe <munalechakona@gmail.com>
2025-09-21 12:51:26 +0200

    added missing imports and basic app structure  as well as render code

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,14 +1,16 @@
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-
-    </>
+    <div>
+      <header className="app-header">
+        <h1>🎙️ Podcast App</h1>
+      </header>
+      <main className="grid">
+        <p>Podcast app</p>
+      </main>
+    </div>
   )
 }
 
